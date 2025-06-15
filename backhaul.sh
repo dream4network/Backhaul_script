159e3ba05e01c9b0125664622182b5443616e2f6
dream4network <120807232+dream4network@users.noreply.github.com>
2025-06-16 01:33:41 +0330

    Update backhaul.sh

--- backhaul.sh
+++ backhaul.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 apt update -y
 apt install wget -y
-wget -O /etc/logo2.sh https://raw.githubusercontent.com/Azumi67/Backhaul_script/refs/heads/main/logo.sh
+wget -4 -O /etc/logo2.sh https://raw.githubusercontent.com/Azumi67/Backhaul_script/refs/heads/main/logo.sh
 chmod +x /etc/logo2.sh
 if [ -f "backhaul.py" ]; then
     rm backhaul.py
 fi
-wget https://github.com/Azumi67/Backhaul_script/releases/download/script/backhaul.py
+wget -4 https://github.com/Azumi67/Backhaul_script/releases/download/script/backhaul.py
 python3 backhaul.py
